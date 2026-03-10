c340968ecd3ae5b7268d3fa7312512c253c1a0aa
MuhammadAwais989 <officialawais989@gmail.com>
2026-03-10 15:04:33 +0500

    update

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -2,27 +2,23 @@ import Subscription from "../models/Subscription.js";
 
 export const saveSubscription = async (req, res) => {
   try {
-    const { browserId, subscription } = req.body;
 
-    const expiresAt = new Date(Date.now() + 10 * 1000);
-    // const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    const { browserId, subscription } = req.body;
 
     let existing = await Subscription.findOne({ browserId });
 
     if (existing) {
       existing.subscription = subscription;
-      existing.expiresAt = expiresAt;
-
       await existing.save();
     } else {
       await Subscription.create({
         browserId,
-        subscription,
-        expiresAt,
+        subscription
       });
     }
 
     res.json({ success: true });
+
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- models/Subscription.js
+++ models/Subscription.js
@@ -3,14 +3,9 @@ import mongoose from "mongoose";
 const subscriptionSchema = new mongoose.Schema({
   browserId: String,
   subscription: Object,
-
   createdAt: {
     type: Date,
     default: Date.now
-  },
-
-  expiresAt: {
-    type: Date
   }
 });
 
